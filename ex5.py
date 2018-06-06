201f5bc5e45d4be5acf10da9a014c259332715bf
shwezinoo <girl24904@gmail.com>
2018-06-05 23:02:36 -0700

    Fifth

---
+++ ex5.py
@@ -0,0 +1,16 @@
+my_name='Zed A. Shaw'
+my_age=35
+my_height=74
+my_weight=180
+my_eyes='Blue'
+my_teeth='White'
+my_hair='Brown'
+print(f"let's talk about{my_name}.")
+print("He's {my_height}inches tall.")
+print("He's {my_weight}pounds heavy")
+print("Accually that's not too heavy")
+print( "He's got {my_eye}eyes and {my_hair}hair.")
+print( "His teeth are usually{my_teeth}depending on the coffee")
+total=my_age+my_height+my_weight
+print("If i add{my_age},{my_height},and{my_weight} I get {total}.")
+
