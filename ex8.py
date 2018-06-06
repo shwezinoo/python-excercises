e0d3f9f696e0427a374b7534e5766029cd3a2302
shwezinoo <girl24904@gmail.com>
2018-06-06 04:08:48 -0700

    eight

---
+++ ex8.py
@@ -0,0 +1,8 @@
+formatter="{}{}{}{}"
+print(formatter.format(1,2,3,4))
+print(formatter.format("one","two","three","four"))
+print(formatter.format(True,False,False,True))
+print(formatter.format(formatter,formatter,formatter,formatter))
+print(formatter.format( "try your","Own text here","May be a poem","Or a song about fear"
+    ))
+
