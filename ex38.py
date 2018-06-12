da84c595189a3270af78bac18c90e220c86ca0bd
shwezinoo <girl24904@gmail.com>
2018-06-12 09:30:51 -0700

    ADD

---
+++ ex38.py
@@ -0,0 +1,17 @@
+ten_things="Apple Orange Crows Telephone Light Sugar"
+print "Wait there's not 10thing in that list"
+stuff=ten_things.split(' ')
+more_stuff=["Day","Night","Song","Frisbee","Corn","Banana","Girl","Boy"]
+while len(stuff)!=10:
+    next_one=more_stuff.pop()
+    print "Adding :",next_one
+    stuff.append(next_one)
+    print "There's %d items now"%len(stuff)
+print "There we go",stuff
+print "Let's do some theings with stuff"
+print stuff[1]
+print stuff[-1]
+print stuff.pop()
+print ' '.join(stuff)
+print '#'.join(stuff[3:5])
+
