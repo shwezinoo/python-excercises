d4f55b4739ea40a7f5ab617ca95e9b8409e0bc51
shwezinoo <girl24904@gmail.com>
2018-06-13 08:50:18 -0700

    ADD

--- ex39.py
+++ ex39.py
@@ -1,15 +1,18 @@
+
 states=[
-        'Oregon': 'OR',
-        'Florida': 'FL',
-        'California': 'CA',
-        'New York': 'NY',
-        'Michigan': 'MI'
+        'Oregon':'OR',
+        'Florida':'FL',
+        'California':'CA',
+        'New York':'NY',
+        'Michigan':'MI'
         ]
+
 cities=[
         'CA': 'San Francisco',
         'MI': 'Detroit',
         'FL': 'Jacksonville'
         ]
+
 cities['NY']='New York'
 cities['OR']='Portland'
 print '_' *10
@@ -21,10 +24,13 @@ print "Florida's abbreviation is",states['Florida']
 print '_'*10
 print "Michigan has",cities[states['Michigan']]
 print "Florida has",cities[states['Florida']]
+
 print '_'*10
+
 for abbrev,city in cities.items():
     print "%s has the city %s"%(abbrev,city)
 print '_'*10
+
 for state,abbrev in states.items():
     print "%s state is abbreviated %s and has city%s"%(state,abbrev,cities[abbrev])
 
